4e024dd66983d816fb26ab5b22331ddda5b29a12
Fefemyster <kenennlara@gmail.com>
2025-11-10 18:56:31 -0600

    api-categorias-terminado

--- api-categorias/server.js
+++ api-categorias/server.js
@@ -5,6 +5,16 @@ const PORT = 3000;
 
 app.use(express.json());
 
+function getCategorias() {
+  const data = fs.readFileSync("./data/categorias.json", "utf-8");
+  return JSON.parse(data);
+}
+
+app.get("/api/categorias", (req, res) => {
+  let categorias = getCategorias();
+  res.status(200).json({ status: 200, message: "success", data: categorias });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
